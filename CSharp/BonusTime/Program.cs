c3c8d039bd011ceaeff8040eef84ada08b8aaade
Rana Bakr <ranabakr110@gmail.com>
2026-07-24 23:39:21 +0300

    Update .gitignor

---
+++ CSharp/BonusTime/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class Kata
+{
+    public static string BonusTime(int salary, bool bonus)
+    {
+        if (bonus)
+            return $"${salary * 10}";
+        else
+            return $"${salary}";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(Kata.BonusTime(10000, true));
+        Console.WriteLine(Kata.BonusTime(25000, false));
+    }
+}
